f2bdfbc0be44725ba66615dbcc1ef445334f83a3
ashraf <ashrafchekintakath@gmail.com>
2025-03-03 18:33:13 +0530

    pp

--- basic/test/test.py
+++ basic/test/test.py
@@ -56,15 +56,3 @@
 # print(l[3])
 # print(l[4])
 
-def generate_pattern(n):
-    result = []
-    for i in range(n, 0, -1):
-        for j in range(n, 0, -1):
-            result.extend([j] * i)
-        result.append(-1)
-    return result
-
-# Example usage
-n = int(input("Enter n: "))  # Input value for n
-print(generate_pattern(n))
-34
