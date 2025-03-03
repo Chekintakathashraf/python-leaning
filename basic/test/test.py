f8eada838111d28b5812080fee757dc1bc09765f
ashraf <ashrafchekintakath@gmail.com>
2025-03-03 17:02:37 +0530

    d

--- basic/test/test.py
+++ basic/test/test.py
@@ -11,3 +11,27 @@
     
 # a()
 # print(x)
+
+# y = "{'1',2,'3'}"
+
+# print(y[0])
+# print(y[1])
+# print(y[5])
+
+
+# x = 10
+
+# x+= 10
+
+# print(x)
+
+# if 1 ==1 : print(True)
+
+# print(True) if 1 ==2 else print(False)
+
+
+list1 = [1,2,3,4]
+
+
+for i,e in enumerate(list1):
+    print(i,'-',e)
