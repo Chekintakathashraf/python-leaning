33dc862251d02c628dadd3d993eebb4469984380
ashraf <ashrafchekintakath@gmail.com>
2025-03-08 00:48:04 +0530

    dd

---
+++ basic/test/closure_vs_nested.py
@@ -0,0 +1,16 @@
+# def outer_function(msg):
+#     def inner_function():  
+#         print(f"Message: {msg}")  
+
+#     inner_function()  
+
+# outer_function("Hello, Nested Function!")  
+
+def outer_closure_function(msg):
+    def inner_closure_function():  
+        print(f"Message: {msg}")  
+
+    return inner_closure_function
+
+call = outer_closure_function("Hello, closure Function!") 
+call() 
