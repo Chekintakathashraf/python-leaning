3793b0dd1c9480859aefa13bce0aa2f24fe4e44a
ashraf <ashrafchekintakath@gmail.com>
2025-03-15 18:23:29 +0530

    jj

--- basic/test/dec.py
+++ basic/test/dec.py
@@ -1,16 +1,23 @@
 def capital(func):
     def inner():
-        print("the func is calling")
+        print("the capital is  calling before")
         msg = func()
-        print("the func is called")
+        print("the capital is called after")
         return msg.upper()
     return inner
 
+def adding(func):
+    def inner():
+        print("the adding is calling before")
+        msg = func()
+        print("the adding is called after")
+        return msg + "!!!"
+    return inner
 
+@adding
+@capital
 def greet():
     return 'Hello!'
 
 
-print(greet())
-greet = capital(greet)
-print(greet())
+print(greet())  
